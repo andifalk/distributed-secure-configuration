45c4ade5d9c27c60791578dd6be20d7b5844ca9b
Andreas Falk <andreas.falk@novatec-gmbh.de>
2017-05-15 23:24:20 +0200

    initial vault config server

---
+++ vault-config-server/src/main/java/com/example/demo/VaultConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultConfigServerApplication.class, args);
+	}
+}
