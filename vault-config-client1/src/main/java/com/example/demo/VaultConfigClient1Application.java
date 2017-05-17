749e7e3b342097ce3e04066d6f8a673b791284ca
Andreas Falk <andreas.falk@novatec-gmbh.de>
2017-05-18 00:56:25 +0200

    add vault config client

---
+++ vault-config-client1/src/main/java/com/example/demo/VaultConfigClient1Application.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class VaultConfigClient1Application {
+
+    @Value("${mysecret:n/a}")
+    private String mySecretKey;
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultConfigClient1Application.class, args);
+	}
+
+	@GetMapping(path = "/")
+	public String index() {
+		return "<h1>Application 1</h1><h2>My secret key=" + mySecretKey + "</h2>";
+	}
+
+}
